55b0063c6868486e40dc1a9d6344cbc3f2a348c5
arodpes <arodpes@gmail.com>
2015-04-30 10:15:01 +0100

    Versión Inicial

---
+++ SignalR02/moveShapeHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalR02
+{
+    [HubName("moveShape")]
+    public class moveShapeHub : Hub
+    {
+        public void moveShape(int x, int y)
+        {
+            Clients.Others.ShapeMoved(x, y);
+        }
+    }
+}
